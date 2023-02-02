757a0902e1d1bd0fcc263ac9829dac1659668724
Luispe <lpbonomi@gmail.com>
2023-02-02 20:35:21 +0100

    configed TS + hardhat

---
+++ backend/hardhat.config.ts
@@ -0,0 +1,23 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {},
+    ETH_MAINNET: {
+      accounts: [`${process.env.PRIVATE_KEY}`],
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+    },
+    ETH_GOERLI: {
+      accounts: [`${process.env.PRIVATE_KEY}`],
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+    },
+  },
+  etherscan: {
+    apiKey: `${process.env.ETHERSCAN_API_KEY}`,
+  },
+};
